f06cc011328c0884a5dc68e198f1a10e2d915895
Mahe0055 <mahe00555@stud.kea.dk>
2023-03-20 10:50:18 +0100

    commit income activity

--- app/src/main/java/com/example/st/IncomeActivity.java
+++ app/src/main/java/com/example/st/IncomeActivity.java
@@ -3,6 +3,7 @@ package com.example.st;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class IncomeActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class IncomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income);
+
+
     }
-}
+
+
+
+
+
+        }
