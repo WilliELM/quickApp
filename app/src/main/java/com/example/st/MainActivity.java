8e133696c99ea9be508822359d725aa57dead266
WilliELM <will585g@stud.kea.dk>
2023-03-20 10:34:40 +0100

    intents working for main, udgifter

--- app/src/main/java/com/example/st/MainActivity.java
+++ app/src/main/java/com/example/st/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.st;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
-
+    public void onClick2(View view){
+        Intent newIntent = new Intent(this, UdgifterActivity.class);
+        startActivity(newIntent);
+    }
 }
--- app/src/main/java/com/example/st/UdgifterActivity.java
+++ app/src/main/java/com/example/st/UdgifterActivity.java
@@ -4,30 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import java.util.ArrayList;
 
-public class Udgifter extends AppCompatActivity {
+public class UdgifterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_udgifter);
-        ArrayList<Integer> nmsArr = new ArrayList<>();
 
+
+    }
+    public void onClick(View view){
+        ArrayList<Integer> nmsArr = new ArrayList<>();
         EditText editText1 = findViewById(R.id.editTextNumber);
         EditText editText2 = findViewById(R.id.editTextNumber2);
         EditText editText3 = findViewById(R.id.editTextNumber3);
         EditText editText4 = findViewById(R.id.editTextNumber4);
 
-        nmsArr.add(Integer.parseInt(editText1.toString()));
-        nmsArr.add(Integer.parseInt(editText2.toString()));
-        nmsArr.add(Integer.parseInt(editText3.toString()));
-        nmsArr.add(Integer.parseInt(editText4.toString()));
+        String value1 = editText1.getText().toString();
+        String value2 = editText2.getText().toString();
+        String value3 = editText3.getText().toString();
+        String value4 = editText4.getText().toString();
 
+        nmsArr.add(Integer.parseInt(value1));
+        nmsArr.add(Integer.parseInt(value2));
+        nmsArr.add(Integer.parseInt(value3));
+        nmsArr.add(Integer.parseInt(value4));
         Intent newIntent = new Intent(this, IncomeActivity.class);
         newIntent.putExtra("arraylist", nmsArr);
         startActivity(newIntent);
     }
+
 }
